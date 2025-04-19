6b5f403ee5c4e9fb4c4bf2361147d555f26dd44c
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-04-19 14:43:51 +0200

    backup

--- app/src/main/java/com/example/a041_stellarforecast/MainActivity.kt
+++ app/src/main/java/com/example/a041_stellarforecast/MainActivity.kt
@@ -2,22 +2,42 @@ package com.example.a041_stellarforecast
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.a041_stellarforecast.databinding.ActivityMainBinding
+import com.example.a041_stellarforecast.domain.model.WeatherReportModel
+import com.example.a041_stellarforecast.presentation.home.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: HomeViewModel by viewModels()
+    // TODO 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        defineRecyclerView()
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    updateCurrentWeather(it.forecast)
+                }
+            }
         }
+
         /** Test synchrone instruction*/
 //        for (i in 1..10000){
 //            Thread.sleep(5)
@@ -33,5 +53,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun updateCurrentWeather(forecast: List<WeatherReportModel>){
+        TODO()
+    }
 
+    private fun defineRecyclerView(){
+        TODO()
+    }
 }
